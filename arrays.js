282711b811a9f049de9b49e8eae763ff3200aa1b
Eliot Howes <ejhowes@gmail.com>
2019-04-22 16:15:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,5 +35,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   var newArray=array.slice(0)
   return newArray
-  return array  
+  return array
 };
