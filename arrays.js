27db4a726f0eb0b48c49e48b64b50674b02f88d8
Eliot Howes <ejhowes@gmail.com>
2019-04-22 14:01:21 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  var array = [1]
+  var element=array.unshift("foo")
+  return array
+};
