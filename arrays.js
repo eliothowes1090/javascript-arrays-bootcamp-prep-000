42d621e97f30baa8c990e975923faa1c03f47e8b
Eliot Howes <ejhowes@gmail.com>
2019-04-22 16:35:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 };
 
 function removeElementFromBeginningOfArray(array) {
-  return array=array.splice(1)
+  return array.splice(1)
 };
