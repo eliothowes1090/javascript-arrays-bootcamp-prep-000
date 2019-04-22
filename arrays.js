cd011d6fa62d27624e32f69f125150b988b5135b
Eliot Howes <ejhowes@gmail.com>
2019-04-22 15:01:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [1]
+  const array = [1]
   var element=array.splice(0, 0, "foo")
   return array
 };
