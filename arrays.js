295e75e2c3a5f674854421c1802fbd8405d2956c
Eliot Howes <ejhowes@gmail.com>
2019-04-22 15:12:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,5 +24,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 
 function accessElementInArray(array, index) {
   var index=array.splice(2, 1)
-  return parseInt.index
+  return number.index
 };
