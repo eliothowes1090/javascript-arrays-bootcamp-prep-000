12b795cf7e88be7ee1a9af209d884e6cb4176b76
Eliot Howes <ejhowes@gmail.com>
2019-04-22 16:42:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,5 +42,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 };
 
 function removeElementFromEndOfArray(array) {
-  return array=array.slice(array.length)
+  return array=array.slice(array.length - 1)
 };
